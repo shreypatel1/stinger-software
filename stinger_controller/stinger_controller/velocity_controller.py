e840978e3b54d5f853ac09fd052c1d47fed9955a
Shrey Patel <shreydpatel1@gmail.com>
2025-11-12 18:08:50 +0000

    Removed unnecesary PID variables

--- stinger_controller/stinger_controller/velocity_controller.py
+++ stinger_controller/stinger_controller/velocity_controller.py
@@ -67,8 +67,6 @@ class VelocityController(Node):
         self.prev_time = None
         self.prev_error_surge = 0
         self.prev_error_yaw = 0
-        self.I_surge_total = 0
-        self.I_yaw_total = 0
 
     def cmd_vel_callback(self, msg: Twist):
         self.cmd_vel = msg
