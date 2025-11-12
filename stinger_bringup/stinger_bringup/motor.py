12c8dfecb386c48b1038e3f8f217516afd724457
Shrey Patel <shreydpatel1@gmail.com>
2025-11-12 23:28:27 +0000

    test

--- stinger_bringup/stinger_bringup/motor.py
+++ stinger_bringup/stinger_bringup/motor.py
@@ -39,17 +39,27 @@ class ESCControlNode(Node):
         
         # Initialize ESCs with first pulse width of 1000us - changed to 1500us
         self.pi.set_servo_pulsewidth(PORT_ESC_PIN, NEUTRAL_PULSE_WIDTH)
-        self.pi.set_servo_pulsewidth(STARBOARD_ESC_PIN, NEUTRAL_PULSE_WIDTH)
         time.sleep(2)  # Allow ESC to register initial pulse
 
         # Quick throttle sweep for arming
         mid_throttle = NEUTRAL_PULSE_WIDTH + (MAX_PULSE_WIDTH - NEUTRAL_PULSE_WIDTH) * 0.4
         self.pi.set_servo_pulsewidth(PORT_ESC_PIN, mid_throttle)
-        self.pi.set_servo_pulsewidth(STARBOARD_ESC_PIN, mid_throttle)
         time.sleep(2)
 
         # Return to neutral
         self.pi.set_servo_pulsewidth(PORT_ESC_PIN, NEUTRAL_PULSE_WIDTH)
+        time.sleep(2)
+
+        # Initialize ESCs with first pulse width of 1000us - changed to 1500us
+        self.pi.set_servo_pulsewidth(STARBOARD_ESC_PIN, NEUTRAL_PULSE_WIDTH)
+        time.sleep(2)  # Allow ESC to register initial pulse
+
+        # Quick throttle sweep for arming
+        mid_throttle = NEUTRAL_PULSE_WIDTH + (MAX_PULSE_WIDTH - NEUTRAL_PULSE_WIDTH) * 0.4
+        self.pi.set_servo_pulsewidth(STARBOARD_ESC_PIN, mid_throttle)
+        time.sleep(2)
+
+        # Return to neutral
         self.pi.set_servo_pulsewidth(STARBOARD_ESC_PIN, NEUTRAL_PULSE_WIDTH)
         time.sleep(2)
 
