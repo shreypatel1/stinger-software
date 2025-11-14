256384396e0c3f895522936a5f051760835ceea2
Shrey Patel <shreydpatel1@gmail.com>
2025-11-14 16:49:50 +0000

    Updated color values

--- stinger_perception/stinger_perception/detection.py
+++ stinger_perception/stinger_perception/detection.py
@@ -45,11 +45,11 @@ class Detection(Node):
         ### END STUDENT CODE
 
         self.red_lower = np.array([0, 100, 100], dtype=np.uint8)
-        self.red_upper = np.array([10, 255, 255], dtype=np.uint8)
-        self.red_lower2 = np.array([170, 100, 100], dtype=np.uint8)
+        self.red_upper = np.array([7, 255, 255], dtype=np.uint8)
+        self.red_lower2 = np.array([173, 100, 100], dtype=np.uint8)
         self.red_upper2 = np.array([179, 255, 255], dtype=np.uint8)
-        self.green_lower = np.array([50, 50, 50], dtype=np.uint8)
-        self.green_upper = np.array([75, 255, 255], dtype=np.uint8)
+        self.green_lower = np.array([55, 50, 50], dtype=np.uint8)
+        self.green_upper = np.array([70, 255, 255], dtype=np.uint8)
 
     def image_callback(self, msg):
         """Process the camera feed to detect red, green, and yellow buoys."""
